cb046032d933febe70afaf9b600938ad4ae068ed
Larisa Moroz <larisa.moroz@gmail.com>
2017-03-06 09:01:23 +0000

    react-router and not found component

---
+++ src/components/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>
+      			NotFound
+      			</h2>
+        <div>
+          <a href="/">Return back to home</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
+import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={ StorePicker } />
+        <Match pattern="/store/:storeId" component={ App } />
+        <Miss component={ NotFound } />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
+
