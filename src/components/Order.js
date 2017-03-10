a0ab693930ddb0e81854ef4684497671c4188d07
Larisa Moroz <larisa.moroz@gmail.com>
2017-03-10 15:00:36 +0000

    Order component better checking for fish available and so on

--- src/components/Order.js
+++ src/components/Order.js
@@ -6,22 +6,33 @@ class Order extends React.Component {
     const {fishes, order} = this.props;
     let total = 0;
     return (
-      <div>
-        <ul className="order-list">
+      <div className="order-wrap">
+        <h2>Your order</h2>
+        <ul className="order">
           {Object.keys(order).map(fish_id => {
-             const item_subtotal = fishes[fish_id].price * order[fish_id];
-             total += item_subtotal;
-             return (
-               <li className="order-item">
-                 <span className="order-item-name">{fishes[fish_id].name}</span>
-                 <span className="order-item-subtotal">{formatPrice(item_subtotal)}</span></li>
+             const fishAvail = fishes[fish_id] && fishes[fish_id].status === 'available';
+             const item_subtotal = (fishes[fish_id].price || 0) * (order[fish_id] || 0);
+             if (fishAvail) {
+               total += item_subtotal;
+               return (
+                 <li className="order-item" key={fish_id}>
+                   <span className="order-item-name">{fishes[fish_id].name}</span>
+                   <span className="order-item-subtotal">{formatPrice(item_subtotal)}</span>
+                 </li>
+               )
+             }
+             else return (
+                 <li className="order-item" key={fish_id}>
+                   Sorry,
+                   {fishes[fish_id] ? fishes[fish_id].name : 'fish'} is no longer available!
+                 </li>
              )
            })}
+          <li className="total">
+            <strong>Total:</strong>
+            <span className="order-total-num">{formatPrice(total)}</span>
+          </li>
         </ul>
-        <div className="order-total">
-          Total:
-          <span className="order-total-num">{formatPrice(total)}</span>
-        </div>
       </div>
     );
   }
